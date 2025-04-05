95f3419dc00132a8766e96a4f24758ac57230aa0
asel <aselbarcelona10@gmail.com>
2025-04-06 01:39:51 +0300

    Init Solution

---
+++ FirstTask/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace FirstTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        FirstTask();
+        SecondTask();
+        ThirdTask();
+    }
+    
+    private static void FirstTask()
+    {
+        Random rnd = new Random();
+        int price = rnd.Next(1, 10000);
+        Console.WriteLine("Cтоимость товара: " + price);
+        
+        int sum = ReadIntFromConsole("Введите сумму имеющихся денежных средств");
+
+        if (sum < price)
+        {
+            Console.WriteLine("Недостаточно денег"); 
+            return;
+        }
+
+        int change = sum - price;
+        Console.WriteLine("Ваша сдача " + change);
+        
+    }
+
+
+    private static void SecondTask()
+    {
+        Console.WriteLine("Введите имя");
+        string name = Console.ReadLine();
+
+        Console.WriteLine("Введите фамилию");
+        string surname = Console.ReadLine();
+
+        int year = ReadIntFromConsole("Введите год рождения");
+        
+        int currentyear = DateTime.Today.Year;
+
+        string age = Convert.ToString(currentyear - year);
+
+        string sayHello = "Привет, " + name + ", " + surname + ", " + age;
+        Console.WriteLine(sayHello);
+    }
+
+    private static void ThirdTask()
+    {
+        int a = ReadIntFromConsole("Введите переменную a");
+        int b = ReadIntFromConsole("Введите переменную b");
+        
+        Console.WriteLine("Переменная a = " + a);
+        Console.WriteLine("Переменная b = " + b);
+
+        a = a + b;
+        b = a - b;
+        a = a - b;
+        
+        Console.WriteLine("Выполнено перемещение");
+        Console.WriteLine("Переменная a = " + a);
+        Console.WriteLine("Переменная b = " + b);
+    }
+
+    private static int ReadIntFromConsole(string message)
+    {
+        int converted = 0;
+        while (converted <= 0)
+        {
+            Console.WriteLine(message);
+            string result = Console.ReadLine();
+            int.TryParse(result, out converted);
+        }
+        return converted;
+    }
+} 
---
+++ SecondTask/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace SecondTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите сторону A");
+        double sideA = Math.Pow(Convert.ToInt32(Console.ReadLine()), 2);
+
+        Console.WriteLine("Введите сторону B");
+        double sideB = Math.Pow(Convert.ToInt32(Console.ReadLine()), 2);
+
+        Console.WriteLine("Введите сторону C");
+        double sideC = Math.Pow(Convert.ToInt32(Console.ReadLine()), 2);
+
+        double maxside = Math.Max(sideA, Math.Max(sideB, sideC));
+
+        string result = "неопределен";
+        
+        // если А наибольшая сторона
+        if (maxside == sideA)
+        {
+            if (sideB + sideC ==  sideA) 
+            {
+                result = "прямоугольный";
+            }
+
+            if (sideB - sideC > sideA) 
+            {
+                result = "остроугольный";
+            }
+
+            if (sideB + sideC < sideA)
+            {
+                result = "тупоугольный";
+            }
+        }
+        // если B наибольшая сторона
+        if (maxside == sideB)
+        {
+            if (sideA + sideC ==  sideB) 
+            {
+                result = "прямоугольный";
+            }
+
+            if (sideA - sideC > sideB) 
+            {
+                result = "остроугольный";
+            }
+
+            if (sideA + sideC < sideB)
+            {
+                result = "тупоугольный";
+            }
+        }
+        
+        // если C наибольшая сторона
+        if (maxside == sideC)
+        {
+            if (sideB + sideA ==  sideC) 
+            {
+                result = "прямоугольный";
+            }
+
+            if (sideB - sideA > sideC) 
+            {
+                result = "остроугольный";
+            }
+
+            if (sideB + sideA < sideC)
+            {
+                result = "тупоугольный";
+            }
+        }
+        
+        Console.WriteLine("Тип треугольника " + result);
+
+    }
+}
+    
