960c371a2a665171823db4a09fa83f9d41d948b1
asel <aselbarcelona10@gmail.com>
2025-04-24 16:02:21 +0300

    SixthTask_Unver

---
+++ SixthTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+namespace SixthTask;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var mango = new Fruits("mango", 135.45m, new DateTime(2025, 05, 01), "a, c");
+        var bread = new Bakery("bread", 30, new DateTime(2025, 05, 03), "безглютеновый");
+        var yogurt = new Dairy("yogurt", 80.49m, new DateTime(2025, 04, 28), 1);
+        
+        mango.Description();
+        Console.WriteLine($"Сладкий этот фрукт: {mango.IsSweet()}");
+        
+        bread.Description();
+        Console.WriteLine($"Свежий этот хлеб: {bread.IsFresh()}");
+        
+        yogurt.Description();
+        Console.WriteLine($"Жирный этот йогурт: {yogurt.IsLowFat()}");
+    }
+}
